1a542be59a67c91ebb61681021b3fdd306578b5e
Gary Bethea <betheagary@gmail.com>
2015-06-12 16:18:13 -0400

    stuff

--- partnerblog/urls.py
+++ partnerblog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     (r'^sourdough_popsicles/', TemplateView.as_view(template_name="sourdough_popsicles.html")),
     (r'^mushrooms/', TemplateView.as_view(template_name="mushrooms.html")),
     (r'^veggie_burgers/', TemplateView.as_view(template_name="veggie_burgers.html")),
+    (r'^hill/', TemplateView.as_view(template_name="hill.html")),
+    (r'winter_fresh_gum_medley', TemplateView.as_view(template_name="winter_fresh_gum_medley.html"))
 )
